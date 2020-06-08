13d08d78f26a0df463e1a29c07973cf710f9a6a0
Satiwan kumar <satiwankumar12@gmail.com>
2020-06-08 16:30:26 +0500

    'first'

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = config.get('MongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGODB_URI||db, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
